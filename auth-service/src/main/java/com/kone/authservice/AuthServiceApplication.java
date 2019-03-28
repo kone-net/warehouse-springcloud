e22a75a818086edf5b2ad0ff08a80e067820e7f7
CongWu <1093312677@qq.com>
2019-03-28 17:03:51 +0800

    create auth service

---
+++ auth-service/src/main/java/com/kone/authservice/AuthServiceApplication.java
@@ -0,0 +1,17 @@
+package com.kone.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableResourceServer
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
