39c9e8ff22beae5963eb9be9399e504e19713c7e
Kone <1093312677@qq.com>
2019-07-12 12:05:23 +0800

    change the config way.

--- user-service/src/main/java/com/kone/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/kone/userservice/UserServiceApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan("com.kone.commonsDao.dao")
+@EnableTransactionManagement
 public class UserServiceApplication {
 
     public static void main(String[] args) {
